8f543ab668f38c46c5f101f19b5e4a2ea14a631c
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-08-16 11:55:32 -0300

    Update index.js

--- exemplo5/index.js
+++ exemplo5/index.js
@@ -1,8 +1,16 @@
+import chalk from 'chalk';
+
+//console.log(chalk.hex('#g4fa54').bgWhite('Trabalhando com NPM'));
+
 let aluno = "Binho";
-let idade = 22;
+let idade = 12;
 
 if (idade >= 18) {
-    console.log (`${aluno} está aprovado!`);
+    console.log(`${aluno} ${chalk.blue.bgWhite('está aprovado!')}`)
 }else {
-    console.log(`${aluno} está reprovado!`);
+    console.log(`${aluno} ${chalk.white.bgRed ('está reprovado!')}`)
 }
+
+//para instalar o chalk utilizamos o npm install chalk
+
+// Chalk é um dos muitos pacotes disponíveis no repositório npmjs.com
